280919a88686a7c427b15578d92f3273996d6eb8
Indra Arianggi Suryaatmaja <indraarianggi@gmail.com>
2018-04-18 14:38:28 +0700

    set routers

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const jwt = require('jsonwebtoken');
 
 const config = require('./config/main');
 
+const User = require('./app/models/user');
+
 // Set port
 let port = process.env.PORT || 3000;
 
@@ -19,7 +21,70 @@ app.use(bodyParser.json());
 
 // Log requests to console
 app.use(morgan('dev'));
-// We will add a quick home page route so I can give a quick demonstration of what morgan does. Add this next.
+
+// Initialize passport for use
+app.use(passport.initialize());
+
+// Connect to database
+mongoose.connect(config.database);
+
+// Bring in defined Passport Strategy
+require('./config/passport')(passport);
+
+const router = express.Router();
+
+// Register new users
+router.post('/register', function(req, res) {
+    if (!req.body.email || !req.body.password) {
+        res.json({ success: false, message: 'Please enter email and password.' });
+    } else {
+        let newUser = new User({
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        // Attempt to save the user
+        newUser.save(function(err) {
+            if (err) return res.json({ success: false, message: 'That email address already exists.' });
+
+            res.json({ success: true, message: 'Successfully created new user.' });
+        });
+    }
+});
+
+// Authenticate the user and get a JSON Web Token to include in the header of future requests.
+router.post('/authenticate', function(req,res) { // login
+    User.findOne({email: req.body.email}, function(err, user) {
+        if (err) throw err;
+
+        if (!user) {
+            res.send({ success: false, message: 'Authentication failed. User not found.' });
+        } else {
+            // Check if password matches
+            user.comparePassword(req.body.password, function(err, isMatch) {
+                if (isMatch && !err) {
+                    // Create token if the password matched and no error was thrown
+                    let token = jwt.sign(
+                        {user}, // payload
+                        config.secret, // secret key
+                        {expiresIn: 10080} // expired time
+                    );
+                    res.json({ success: true, token: 'JWT ' + token });
+                } else {
+                    res.json({ success: false, message: 'Authentication failed. Password did not match.' });
+                }
+            });
+        }
+    });
+});
+
+// Protect dashboard route with JWT
+router.get('/dashboard', passport.authenticate('jwt', {session:false}), function(req, res) {
+    res.send('It worked! User id is: ' + req.user._id + '.');
+});
+
+// Set url for API group routes
+app.use('/api', router);
 
 // Home route. We'll end up changing this to our main front end index later.
 app.get('/', function(req, res) {
